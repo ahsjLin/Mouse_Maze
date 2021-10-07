576ea3530ddba54c8c1907a20a14cd7e336f7759
tzyrrxr <sjlin0827@outlook.com>
2021-10-07 11:49:54 +0800

    2021-10-07_11-49

--- findExitThread.cpp
+++ findExitThread.cpp
@@ -5,24 +5,6 @@
 
 #include "findExitThread.h"
 #pragma package(smart_init)
-
-#define ROW 32
-#define COLUMN 32
-#define MAX_STACK 1000
-extern int ori[ROW][COLUMN];
-extern int maze[ROW][COLUMN];
-extern int in_x;
-extern int in_y;
-extern int out_x;
-extern int out_y;
-extern int top;
-void push(int x, int y, int direction);
-void pop();
-void showMaze();
-void reDrawPath();
-int checkCanRun(int go);
-void sendPush(int direction);
-void findEixt();
 //---------------------------------------------------------------------------
 
 //   Important: Methods and properties of objects in VCL can only be
@@ -46,6 +28,7 @@ __fastcall findExitThread::findExitThread(bool CreateSuspended)
 void __fastcall findExitThread::Execute()
 {
 	//---- Place thread code here ----
+    FreeOnTerminate = true;
     findEixt();
 }
 //---------------------------------------------------------------------------
